a71e56e75e2a02781c08a2f751de716242233d2c
Andrew An <anandrew1995@gmail.com>
2020-06-14 22:18:11 -0400

    Updated gitignore

---
+++ build/precache-manifest.2622c332c07c90dc9d178e3605eeb9d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e2f71a08937be8b759f300f4db59af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a8c3ab37a44e13a2894",
+    "url": "/static/css/main.1f606415.chunk.css"
+  },
+  {
+    "revision": "145ce1f03bfc95010ca4",
+    "url": "/static/js/2.37f28ee0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37f28ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8c3ab37a44e13a2894",
+    "url": "/static/js/main.1335e8db.chunk.js"
+  },
+  {
+    "revision": "a0d69f97458787781d45",
+    "url": "/static/js/runtime-main.ab52fb89.js"
+  },
+  {
+    "revision": "418d6668afcddb7714104f43bfb39657",
+    "url": "/static/media/AnonymousPro-Regular.418d6668.ttf"
+  },
+  {
+    "revision": "03ef685b34f3356631c3f60c91917002",
+    "url": "/static/media/CourierPrime-Regular.03ef685b.ttf"
+  },
+  {
+    "revision": "69c53a329bc1415532f5b30db5ab13df",
+    "url": "/static/media/uw-logo.69c53a32.png"
+  }
+]);
