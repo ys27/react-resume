1a043cb8f0a0c8a465a3a80b4b46a991b3695b47
Andrew An <anandrew1995@gmail.com>
2023-08-23 19:04:22 -0400

    Update experience

--- src/components/ProfessionalExperience/Data.js
+++ src/components/ProfessionalExperience/Data.js
@@ -1,64 +1,65 @@
 const Data = [
   {
     title: {
-      company: "Fiserv",
-      durations: ["June 2021 - Present"],
-      roles: ["Full-stack Developer", "Team Lead"],
+      company: 'Fiserv',
+      durations: ['June 2021 - Present'],
+      roles: ['Full-stack Developer', 'Team Lead'],
     },
     descriptions: [
       {
         mainLine: {
-          project: "Developer Studio",
-          stack: ["MERN", "GraphQL", "Sass"],
+          project: 'Developer Studio',
+          stack: ['MERN', 'GraphQL', 'Sass'],
         },
         subLines: [
-          "Ownership of features and end-to-end architecture to implementation.",
-          "Resolved pen tests and security vulnerabilities.",
-          "Managed external tenants with guidance, set-up, and issues.",
-          "Managed internal team with sprint management.",
+          'Built production-level financial features to attract and contract with external companies.',
+          'Ownership of features - from architecture to implementation.',
+          'Resolved pen tests and security vulnerabilities.',
+          'Collaboration with external teams to co-develop features.',
+          'Managed internal squad & sprint management.',
         ],
       },
     ],
   },
   {
     title: {
-      company: "IBM",
-      durations: ["Oct 2018 - Feb 2021"],
-      roles: ["Full-stack Developer", "Scrum Master"],
+      company: 'IBM',
+      durations: ['Oct 2018 - Feb 2021'],
+      roles: ['Full-stack Developer', 'Scrum Master'],
     },
     descriptions: [
       {
         mainLine: {
-          project: "Safer Payments",
-          stack: ["React/Redux/Sagas", "TS", "Sass", "C++", "Python"],
+          project: 'Safer Payments',
+          stack: ['React/Redux/Sagas', 'TS', 'Sass', 'C++', 'Python'],
         },
-        subLines: ["Prod level full-stack feature development/maintenance."],
+        subLines: ['Prod level full-stack feature development/maintenance.'],
       },
       {
         mainLine: {
-          project: "Entity Enrichment",
-          stack: ["MERN/Redux", "Custom Packages"],
+          project: 'Entity Enrichment',
+          stack: ['MERN/Redux', 'Custom Packages'],
         },
         subLines: [
-          "Designed and built various entity enrichment methods for prod level APIs & demo UI.",
-          "Owned and managed a stand-alone entity enrichment micro-service.",
-          "Wrote a published paper for novel entity enrichment ideas.",
+          'Designed and built various entity enrichment methods for prod level APIs & demo UI.',
+          'Owned and managed a stand-alone entity enrichment micro-service.',
+          'Wrote a published paper for novel entity enrichment ideas.',
         ],
       },
       {
         mainLine: {
-          project: "Transaction List Screening",
-          stack: ["React/Redux/Sagas", "Mongo", "Flask"],
+          project: 'Transaction List Screening',
+          stack: ['React/Redux/Sagas', 'Mongo', 'Flask'],
         },
-        subLines: ["Prod level full-stack feature development/maintenance."],
+        subLines: ['Prod level full-stack feature development/maintenance.'],
       },
       {
         mainLine: {
-          project: "Open Banking",
-          stack: ["React/Redux", "Java", "CouchDB"],
+          project: 'Open Banking',
+          stack: ['React/Redux', 'Java', 'CouchDB'],
         },
         subLines: [
-          "Designed and built micro-services with authenticated APIs + UI to visualize concepts.",
+          'Designed and built micro-services with authenticated APIs + UI to visualize concepts.',
         ],
       },
     ],
